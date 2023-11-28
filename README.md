1912631e6a9d97784bfbff79f1271439d15d02a0
Prizmin <helloworldpy103@gmail.com>
2023-11-27 19:59:00 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Pepsearch
---
+++ static/funFact.js
@@ -0,0 +1,20 @@
+async function getRandomPhrase() {
+    console.log("Fetching a fun fact")
+    var randomPhrase;
+    await fetch("https://api.api-ninjas.com/v1/facts?limit=1", {
+       "headers": {
+          "content-type": "application/json",
+          "x-api-key": "1HHHukaNEC5Kh3iDOLeZjQ==nxxKAnwy9XbK9bYM"
+       },
+       "method": "GET",
+       "credentials": "omit",                  
+    })
+    .then(response => response.json())
+    .then(json => randomPhrase = json[0]['fact']) 
+    
+    console.log(randomPhrase)
+    
+    document.getElementById("phrase").innerHTML = randomPhrase
+  }
+  
+ window.onload = getRandomPhrase;
